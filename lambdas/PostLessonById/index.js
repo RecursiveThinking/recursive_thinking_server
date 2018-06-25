ff3aa55149385d071849b63f328d4dc548e9f1f5
AvsCode <avsean@gmail.com>
2018-06-25 15:51:36 -0700

    add postLessonById functionality

--- lambdas/PostLessonById/index.js
+++ lambdas/PostLessonById/index.js
@@ -5,37 +5,38 @@ exports.PostLessonById = (event, context, callback) => {
 
     console.log(JSON.stringify(event));
 
-    const pathParam = event.pathParameters.id;
-    const userName = event.requestContext.authorizer.claims["cognito:username"];
-
-    if (pathParam !== userName) {
-        return context.succeed({
-            statusCode: 403,
-            body: JSON.stringify({ message: 'Forbidden.' }),
-            headers: {'Content-Type': 'text/plain'}
-        });
-    }
+    // const pathParam = event.pathParameters.id;
+    // const createdBy = event.requestContext.authorizer.claims.sub;
 
     const params = {
         Item: {
             ...JSON.parse(event.body),
-            Username: event.requestContext.authorizer.claims["cognito:username"]
+            createdBy: event.requestContext.authorizer.claims.sub
         },
-        TableName : process.env.TABLE
+        TableName: process.env.TABLE
     };
 
-    dynamodb.put(params, function(err, data) {
+    dynamodb.put(params, function (err, data) {
         if (err) {
+            console.log(err);
             return context.succeed({
                 statusCode: 501,
-                body: JSON.stringify({ message: 'There was an error when calling DynamoDB' }),
-                headers: {'Content-Type': 'text/plain'}
+                body: JSON.stringify({
+                    message: 'There was an error when calling DynamoDB'
+                }),
+                headers: {
+                    'Content-Type': 'text/plain',
+                    'Access-Control-Allow-Origin': '*'
+                }
             });
         } else {
             return context.succeed({
                 statusCode: 200,
                 body: JSON.stringify(data),
-                headers: {'Content-Type': 'application/json'}
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
             });
         }
     });
