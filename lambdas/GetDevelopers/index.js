55d516b66059d77527cd607258c46c0009353415
AvsCode <avsean@gmail.com>
2018-06-25 17:12:09 -0700

    add getLessons functionality, add more error logging to get Developers

--- lambdas/GetDevelopers/index.js
+++ lambdas/GetDevelopers/index.js
@@ -4,23 +4,30 @@ const dynamodb = new aws.DynamoDB.DocumentClient();
 exports.GetDevelopers = (event, context, callback) => {
 
     const params = {
-        TableName : process.env.TABLE
+        TableName: process.env.TABLE
     };
 
-    dynamodb.scan(params, function(err, data) {
+    dynamodb.scan(params, function (err, data) {
         if (err) {
             context.succeed({
                 statusCode: 501,
-                body: JSON.stringify({ message: 'There was an error when calling DynamoDB' }),
-                headers: {'Content-Type': 'text/plain',
-                'Access-Control-Allow-Origin': '*'}
+                body: JSON.stringify({
+                    message: 'There was an error when calling DynamoDB',
+                    error: err
+                }),
+                headers: {
+                    'Content-Type': 'text/plain',
+                    'Access-Control-Allow-Origin': '*'
+                }
             });
         } else {
             context.succeed({
                 statusCode: 200,
                 body: JSON.stringify(data),
-                headers: {'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'}
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
             });
         }
     });
