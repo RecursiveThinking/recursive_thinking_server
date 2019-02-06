0d682425cbd91b828f9220598da04f436d718fc5
Seth Borne <sethborne@gmail.com>
2019-02-06 15:50:39 -0800

    adds IQbyid lambdas

--- lambdas/RTWInterviewQuestionDeleteById/index.js
+++ lambdas/RTWInterviewQuestionDeleteById/index.js
@@ -1,7 +1,54 @@
+// {
+//   "pathParameters": {
+//     "id": "870d7070-8e21-11e8-ad35-63bc93d0cda5s"
+//   }
+// }
 //access SDK
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB({region: 'us-west-2', apiVerson: '2012-08-10'})
+const dynamodb = new AWS.DynamoDB({
+  region: 'us-west-2', 
+  apiVerson: '2012-08-10'
+})
 
 exports.InterviewQuestionDeleteById = (event, context, callback) => {
-
+  console.log('event @ IntQuestById Delete: ', event);
+  console.log('context @ IntQuestById Delete: ', context);
+  
+  const params = {
+    Key: {
+      'Id': {
+        // S: event.Id
+        S: event.pathParameters.id
+      }
+    },
+    TableName: process.env.Table
+  }
+  dynamodb.deleteItem(params, function(err, intQuestDel){
+    let response = {};
+    if(err){
+      response.statusCode = 501;
+      response.body = JSON.stringify({
+        message: 'There was an error calling DynamoDB',
+        error: err
+      })
+      response.headers = {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*'
+      }
+      console.log('err', response);
+      callback(response)
+    } else {
+      // wont return anything...?
+      
+      // 
+      response.statusCode = 200;
+      response.body = JSON.stringify({});
+      response.headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+      // cb
+      callback(null, response)
+    }
+  })
 }
--- lambdas/RTWInterviewQuestionGetById/index.js
+++ lambdas/RTWInterviewQuestionGetById/index.js
@@ -1,7 +1,55 @@
+// {
+//   "pathParameters": {
+//     "id": "870d7070-8e21-11e8-ad35-63bc93d0cda5"
+//   }
+// }
 //access SDK
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB({region: 'us-west-2', apiVerson: '2012-08-10'})
+const dynamodb = new AWS.DynamoDB({
+  region: 'us-west-2', 
+  apiVerson: '2012-08-10'
+})
 
 exports.InterviewQuestionGetById = (event, context, callback) => {
-
+  // TODO implement
+  console.log('event @ IntQuest Get', event);
+  console.log('context @ IntQuest Get', context);
+  
+  const params = {
+    Key: {
+      'Id': {
+        // S: event.id
+        S: event.pathParameters.id
+      }
+    },
+    TableName: process.env.TABLE
+  }
+  dynamodb.getItem(params, function(err, intQuest){
+    let response = {}
+    if(err){
+      response.statusCode = 501;
+      response.body = JSON.stringify({
+        message: 'There was an Error Calling DynamoDB',
+        error: err
+      })
+      response.headers = {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*'
+      }
+      console.log('err', response);
+      callback(response);
+    } else {
+      // good intQuest so unmarshall it
+      const intQuestUnmarshalled = AWS.DynamoDB.Converter.unmarshall(intQuest.Item);
+      // build response object
+      response.statusCode = 200;
+      response.body = JSON.stringify(intQuestUnmarshalled);
+      response.headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+      // cb response
+      callback(null, response);
+    }
+  })
 }
