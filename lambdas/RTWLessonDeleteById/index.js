75a78ca3ec9157696c55d1d74e4cf0a55ff45968
Seth Borne <sethborne@gmail.com>
2019-02-19 12:31:22 -0800

    updates lessonDelete lambda to DocClient and returns del obj

--- lambdas/RTWLessonDeleteById/index.js
+++ lambdas/RTWLessonDeleteById/index.js
@@ -3,13 +3,9 @@
 //     "id": "8c57c8d0-8e19-11e8-924a-a70245d1837e"
 //   }
 // }
-//access SDK
+// access SDK
 const AWS = require('aws-sdk');
-// const dynamodb = new AWS.DynamoDB.DocumentClient();
-const dynamodb = new AWS.DynamoDB({
-  region: 'us-west-2', 
-  apiVerson: '2012-08-10'
-})
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.LessonDeleteById = (event, context, callback) => {
   console.log('event @ LessonDeleteId Delete: ', event);
@@ -17,19 +13,13 @@ exports.LessonDeleteById = (event, context, callback) => {
   console.log('callback @ LessonDeleteId Delete: ', callback);
   
   const params = {
-    // Key: {
-    //   'Id': event.pathParameters.id
-    // },
-    // ReturnValues: "ALL_OLD",
     Key: {
-      'Id': {
-        // S: event.id
-        S: event.pathParameters.id
-      }
+      'Id': event.pathParameters.id
     },
+    ReturnValues: "ALL_OLD",
     TableName: process.env.TABLE
   }
-  dynamodb.deleteItem(params, function(err, lessonDel){
+  dynamodb.delete(params, function(err, lessonDel){
     if(err){
       let error = {}
       error.statusCode = 501;
@@ -46,15 +36,10 @@ exports.LessonDeleteById = (event, context, callback) => {
       callback(err)
     } else {
       let response = {};
-      // response.statusCode = 200;
-      // response.body = JSON.stringify(lessonDel.Attributes)
-      // response.headers = {
-      //   'Content-Type': 'application/json',
-      //   'Access-Control-Allow-Origin': '*'
-      // }
+      console.log('Item Deleted Successfully')
       // build response object
       response.statusCode = 200;
-      response.body = JSON.stringify(lessonDel);
+      response.body = JSON.stringify(lessonDel.Attributes)
       response.headers = {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
