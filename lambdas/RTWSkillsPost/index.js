7f46b5233d6950db3b294e5a7792715667d940c6
Seth Borne <sethborne@gmail.com>
2019-02-19 10:33:06 -0800

    update lambdas skills

--- lambdas/RTWSkillsPost/index.js
+++ lambdas/RTWSkillsPost/index.js
@@ -1,39 +1,46 @@
 //access SDK
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB({
-  region: 'us-west-2', 
-  apiVerson: '2012-08-10'
-})
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.SkillsPost = (event, context, callback) => {
   // const userId = event.requestContext.authorizor.claims.sub;
   const body = typeof event.body === "string" ? JSON.parse(event.body) : event.body
   console.log('event @ postProfileSkills: ', event);
   console.log('event.body @ postProfileSkills: ', event.body)
+  console.log('body @ postProfileSkills: ', body)
   console.log('context @ postProfileSkills: ', context);
 
   const params = {
-    Item: body,
+    ReturnValues: "ALL_NEW",
     TableName: process.env.TABLE
   }
+  
+  for(let key in body){
+    params[key] = body[key]
+  }
+  
   console.log('params @ postProfileSkills: ', params)
-  dynamodb.putItem(params, function(err, profileSkillToPost){    
-    let response = {};
+  
+  dynamodb.update(params, function(err, skillToPost){    
+    
     if(err){
-      response.statusCode = 502;
-      response.body = JSON.stringify({
+      let error = {};
+      error.statusCode = 501;
+      error.body = JSON.stringify({
         message: 'There was an error calling Dynamo DB',
         error: err
       })
-      response.headers = {
+      error.headers = {
         'Content-Type': 'text/plain',
         'Access-Control-Allow-Origin': '*'
       }
-      callback(response)
+      // callback(error)
+      callback(err)
     } else {
-      console.log('profileSkillToPost', profileSkillToPost)
+      let response = {};
+      console.log('profileSkillToPost', skillToPost)
       response.statusCode = 200;
-      response.body = JSON.stringify(profileSkillToPost);
+      response.body = JSON.stringify(skillToPost.Attributes);
       response.headers = {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
