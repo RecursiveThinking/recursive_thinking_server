882c6bd57a847418f512aa2b923f72dbbdac9c82
Seth Borne <sethborne@gmail.com>
2019-02-23 12:11:47 -0800

    updates user put by id lambda

--- lambdas/RTWUserPutById/index.js
+++ lambdas/RTWUserPutById/index.js
@@ -5,11 +5,54 @@
 // }
 //access SDK
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB({
-  region: 'us-west-2', 
-  apiVerson: '2012-08-10'
-})
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.UserPutById = (event, context, callback) => {
-
+  const body = typeof event.body === "string" ? JSON.parse(event.body) : event.body
+  console.log('event @ putUsers: ', event);
+  console.log('event.body @ putUsers: ', event.body)
+  console.log('body @ putUsers: ', body);
+  console.log('context @ putUsers: ', context);
+  
+  const params = {
+    Key: {
+      'userId': body.userId
+    },
+    ReturnValues: "ALL_NEW",
+    TableName: process.env.TABLE
+  }
+  
+  for(let key in body){
+    params[key] = body[key]
+  }
+  
+  console.log('params @ putUsers: ', params);
+  
+  dynamodb.update(params, function(err, userToEdit){
+    if(err){
+      let error = {}
+      error.statusCode = 501;
+      error.body = JSON.stringify({
+        message: 'There was an error calling Dynamo DB',
+        error: err
+      })
+      error.headers = {
+        'Content-Type': 'text/plain', 
+        'Access-Control-Allow-Origin': '*'
+      }
+      // callback(error);
+      callback(err);
+    } else {
+      let response = {};
+      console.log('userToEdit: ', userToEdit);
+      // build response
+      response.statusCode = 200;
+      response.body = JSON.stringify(userToEdit.Attributes);
+      response.headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+      callback(null, response);
+    }
+  })
 }
